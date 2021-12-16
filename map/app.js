4214adad96a9e89b0c834c9b7e45c93e336d2344
adhikarisushant <sushant.adhikari.619@gmail.com>
2021-12-16 09:21:39 +0545

    map, unique values

---
+++ map/app.js
@@ -0,0 +1,43 @@
+// MAP Method
+
+const people = [
+    {
+        name: 'bob',
+        age: 20,
+        position: 'developer',
+    },
+    {
+        name: 'anna',
+        age: 25,
+        position: 'designer',
+    },
+    {
+        name: 'susy',
+        age: 30,
+        position: 'the boss',
+    },
+    {
+        name: 'john',
+        age: 26,
+        position: 'intern',
+    },
+];
+
+
+const getAges = (person) => person.age * 2;
+
+const ages = people.map(getAges);
+console.log(ages);
+
+const newPeople = people.map((item) => {
+    return {
+        firstName: item.name.toUpperCase(),
+        oldAge: item.age + 20,
+    };
+});
+console.log(newPeople);
+
+const names = people.map((person) => `<h2>${person.name}</h2>`);
+const result = document.querySelector('#result');
+
+result.innerHTML = names.join('')
